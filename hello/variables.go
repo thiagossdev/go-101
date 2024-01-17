7c6dd5b57f792213979995d40043c5dd6f70f311
Thiago Sousa Santos <thiago@tartigrado.com>
2024-01-17 11:15:16 -0300

    Add menu options to variables.go
    Menu options have been implemented for a more interactive user interface. The user can now choose to start the monitor, display logs, or exit the program. An integer input corresponding to the user's choice is also properly handled and logged.

--- hello/variables.go
+++ hello/variables.go
@@ -11,4 +11,13 @@ func main() {
 	fmt.Println("Hello,", name)
 	fmt.Println("This program is in version", version)
 	fmt.Println("The type of name var is", reflect.TypeOf(name))
+
+	fmt.Println("1- Start monitor")
+	fmt.Println("2- Display logs")
+	fmt.Println("0- Exit the program")
+
+	var command int
+	fmt.Scanf("%d", &command)
+
+	fmt.Println("\nThe chose command was", command)
 }
