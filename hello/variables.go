f57adfb018a3ad11acaa71fdf3aba7a465e7d200
Thiago Sousa Santos <thiago@tartigrado.com>
2024-01-17 11:10:22 -0300

    Use shortcuts to declare vars

--- hello/variables.go
+++ hello/variables.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	var name = "Thiago Sousa Santos"
-	var version float32 = 0.1
+	name := "Thiago Sousa Santos"
+	version := 0.1
 	fmt.Println("Hello,", name)
 	fmt.Println("This program is in version", version)
 	fmt.Println("The type of name var is", reflect.TypeOf(name))
